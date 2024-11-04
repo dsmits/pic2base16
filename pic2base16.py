765bd3b9586fa28bae29b204d155d07417f9a8ab
Djura Smits <djura.smits@gmail.com>
2024-11-04 16:03:00 +0100

    Made things work

--- pic2base16.py
+++ pic2base16.py
@@ -1,6 +1,14 @@
+from pathlib import Path
+
+import base16_colorlib
+from PIL.Image import Palette
+from PIL.ImageFile import ImageFile
 from base16_colorlib import catppuccin_mocha
 from PIL import ImageColor, Image
 from PIL.ImagePalette import ImagePalette
+
+NUM_COLORS = 16
+TARGET_WIDTH = 256
 
 
 def extract_palette(base16_scheme: dict[str, str]):
@@ -10,15 +18,30 @@ def extract_palette(base16_scheme: dict[str, str]):
             rgb = ImageColor.getrgb(value)
             palette.extend(rgb)
     print(palette)
-    return ImagePalette(palette=palette)
+    return palette
 
 
-def convert(input_, target):
+def get_target_size(im: ImageFile):
+    scale = TARGET_WIDTH / im.width
+
+    return TARGET_WIDTH, int(im.height * scale)
+
+
+def convert(input_, target: Path, scheme_name, overwrite=False):
+    if target.exists() and not overwrite:
+        raise FileExistsError(f"{target} already exists")
+
+    scheme = getattr(base16_colorlib, scheme_name)
+    palette = extract_palette(scheme)
+
     im = Image.open(input_)
+    target_size = get_target_size(im)
+    im = im.resize(target_size)
 
-    palette = extract_palette(catppuccin_mocha)
+    paletteImage = Image.new("P", (1, 1))
 
-    converted = im.convert(mode="P", palette=palette)
+    paletteImage.putpalette(palette)
+    converted = im.quantize(palette=paletteImage, dither=2)
 
     converted.save(target)
 
--- tests/test_pic2base16.py
+++ tests/test_pic2base16.py
@@ -1,9 +1,11 @@
 from pic2base16 import convert
 from pathlib import Path
+
 TEST_PIC = Path("./tests/test_pic.jpg")
+TEST_SCHEME = "solarized"
 
 
 def test_convert_runs():
-    result_file = TEST_PIC.parent/"result.png"
+    result_file = TEST_PIC.parent / "result.bmp"
 
-    convert(TEST_PIC, result_file)
+    convert(TEST_PIC, result_file, TEST_SCHEME)
